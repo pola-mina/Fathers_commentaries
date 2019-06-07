6f71a865d7c494cdd3fd867ac14e7c989455e6b6
pola-mina <polamoussa@gmail.com>
2019-06-07 10:24:25 +0200

    migrate commentaries

--- db/migrate/20190606160624_create_father_commentaries.rb
+++ db/migrate/20190606160624_create_father_commentaries.rb
@@ -1,8 +1,12 @@
 class CreateFatherCommentaries < ActiveRecord::Migration[5.2]
   def change
     create_table :father_commentaries do |t|
-
       t.timestamps
+      t.column :english_commentary, :text
+      t.column :arabic_commentary, :text
+      t.column :searchable_english_commentary, :text
+      t.column :searchable_arabic_commentary, :text
+      t.column :father_id, :int
     end
   end
 end
